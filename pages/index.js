086895e86f6f5eaf1b17874b7cb53289c50c0eed
Lucas Eduardo <luklucas42@gmail.com>
2022-05-27 12:52:06 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Card from "../pages/components/Card";
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export async function getStaticProps() {
+    const maxPokemons = 251;
+    const api = "https://pokeapi.co/api/v2/pokemon/"
+
+    const dados = await fetch(`${api}/?limit=${maxPokemons}`);
+
+    const data = await dados.json();
+
+    console.log(data.results);
+
+    data.results.forEach((item, index) => {
+      item.id = index + 1;
+    });
+
+    return {
+      props: {
+        pokemons: data.results,
+      }
+    }
+}
+
+export default function Home({pokemons}) {
+  return (
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokeNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </>
+  )
+}
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import styles from '../styles/About.module.css';
+
+export default function Sobre() {
+    return(
+        <div className={styles.about}>
+            <h1>Sobre</h1>
+            <p>Lorem, algo</p>
+            <Image src="/images/charizard.png" width="300" height="300" alt="charizard"/>
+        </div>
+    )
+}
